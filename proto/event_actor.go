d0c551cef3818382c83410863666186a2ef1d71e
jloup <jeanloup.jamet@gmail.com>
2025-02-17 22:13:20 +0100

    update

--- proto/event_actor.go
+++
@@ -1,5 +0,0 @@
-package proto
-
-type EventActor struct {
-	Handle string
-}
--- space_validation.go
+++ space_validation.go
@@ -6,44 +6,50 @@ import (
 	"github.com/ebenaum/thekeeper/proto"
 )
 
+type Actor struct {
+	Handle  string
+	Players map[string]struct{}
+}
+
 type SpaceValidation struct {
 	Handles    map[string]int64
 	Permission Permission
 	PlayersIDs map[string]struct{}
-	Actors     map[int64]struct {
-		Handle  string
-		Players map[string]struct{}
-	}
+	Actors     map[int64]Actor
 }
 
 func (s *SpaceValidation) Process(sourceActorID int64, event *proto.Event) error {
 	switch v := event.Msg.(type) {
-	case *proto.EventActor:
-		if _, exists := s.Handles[v.Actor.Handle]; exists {
+	case *proto.Event_SeedActor:
+		if _, exists := s.Handles[v.SeedActor.Handle]; exists {
 			return fmt.Errorf("handler already exists")
 		}
+
 		if s.Actors[sourceActorID].Handle != "" {
 			return fmt.Errorf("handler already exists")
 		}
 
-		s.Handles[v.Actor.Handle] = sourceActorID
-		s.Actors[sourceActorID].Handle = v.Actor.Handle
+		s.Handles[v.SeedActor.Handle] = sourceActorID
+		s.Actors[sourceActorID] = Actor{
+			v.SeedActor.Handle,
+			make(map[string]struct{}),
+		}
 
 		return nil
 	case *proto.Event_Permission:
 		return s.Permission.Process(sourceActorID, v.Permission)
-	case *proto.Event_Seed:
-		actorID := s.Handles[v.Seed.Handle]
+	case *proto.Event_SeedPlayer:
+		actorID := s.Handles[v.SeedPlayer.Handle]
 		if sourceActorID != actorID && s.Permission.actors[sourceActorID] != PermissionOrga {
 			return fmt.Errorf("not authorized")
 		}
 
-		if _, exists := s.PlayersIDs[v.Seed.PlayerId]; exists {
+		if _, exists := s.PlayersIDs[v.SeedPlayer.PlayerId]; exists {
 			return fmt.Errorf("player already exists")
 		}
 
-		s.PlayersIDs[v.Seed.PlayerId] = struct{}{}
-		s.Actors[actorID].Players[v.Seed.PlayerId] = struct{}{}
+		s.PlayersIDs[v.SeedPlayer.PlayerId] = struct{}{}
+		s.Actors[actorID].Players[v.SeedPlayer.PlayerId] = struct{}{}
 
 		return nil
 	default:
@@ -56,22 +62,20 @@ type SpacePlayer struct {
 	Permission Permission
 	ActorID    int64
 	Events     []*proto.Event
-	Actors     map[int64]struct {
-		Handle string
-	}
+	Actors     map[int64]string
 }
 
 func (s *SpacePlayer) Process(sourceActorID int64, event *proto.Event) error {
 	switch v := event.Msg.(type) {
-	case *proto.EventActor:
-		s.Handles[v.Actor.Handle] = sourceActorID
-		s.Actors[sourceActorID].Handle = v.Actor.Handle
+	case *proto.Event_SeedActor:
+		s.Handles[v.SeedActor.Handle] = sourceActorID
+		s.Actors[sourceActorID] = v.SeedActor.Handle
 
 		return nil
 	case *proto.Event_Permission:
 		return s.Permission.Process(sourceActorID, v.Permission)
-	case *proto.Event_Seed:
-		actorID := s.Handles[v.Seed.Handle]
+	case *proto.Event_SeedPlayer:
+		actorID := s.Handles[v.SeedPlayer.Handle]
 		if actorID == s.ActorID || s.Permission.actors[s.ActorID] == PermissionOrga {
 			s.Events = append(s.Events, event)
 		}
