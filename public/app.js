8032f8e80c3c9c2e1f03bf3b540c5ec645032329
jloup <jeanloup.jamet@gmail.com>
2025-05-25 18:56:19 +0200

    feat: implement hover detection for improved UI interactions and update related styles

--- public/app.js
+++ public/app.js
@@ -1902,6 +1902,38 @@ async function informations() {
   }
 }
 
+function watchForHover() {
+  // lastTouchTime is used for ignoring emulated mousemove events
+  // that are fired after touchstart events. Since they're
+  // indistinguishable from real events, we use the fact that they're
+  // fired a few milliseconds after touchstart to filter them.
+  let lastTouchTime = 0;
+
+  function enableHover() {
+    const now = new Date();
+    // @ts-ignore
+    if (now - lastTouchTime < 500) return;
+    document.body.classList.add("hasHover");
+  }
+
+  function disableHover() {
+    document.body.classList.remove("hasHover");
+  }
+
+  function updateLastTouchTime() {
+    // @ts-ignore
+    lastTouchTime = new Date();
+  }
+
+  document.addEventListener("touchstart", updateLastTouchTime, true);
+  document.addEventListener("touchstart", disableHover, true);
+  document.addEventListener("mousemove", enableHover, true);
+
+  enableHover();
+}
+
+watchForHover();
+
 switch (window.location.pathname) {
   case "/personnage.html":
   case "/personnage":
