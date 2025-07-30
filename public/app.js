87da98576b059d8e76f80d5fa1eef90d9013be46
jloup <jeanloup.jamet@gmail.com>
2025-07-30 13:11:37 +0200

    fix: remove skills count from character display in the view

--- public/app.js
+++ public/app.js
@@ -2198,7 +2198,7 @@ async function theview2() {
       const character = state.data.characters[characterId];
 
       const characterLiElement = document.createElement("li");
-      characterLiElement.innerHTML = `<a class="a-underline" href="/personnage.html?characterId=${characterId}" target="blank_">${character.name} x${character.skills[key]}</a>`;
+      characterLiElement.innerHTML = `<a class="a-underline" href="/personnage.html?characterId=${characterId}" target="blank_">${character.name}</a>`;
 
       ulElement.appendChild(characterLiElement);
     });
